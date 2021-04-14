03e999d49247744156c828bf70c3171abcaa5204
Skylinker <skylinkertu33@gmail.com>
2021-04-14 10:51:46 +0700

    Preparing the application for deployment

---
+++ precache-manifest.314aa058c40ff9927dc84cac9e32b53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9bb5b6bbbc111bd58b",
+    "url": "/vue-calendar/css/app.fae415a4.css"
+  },
+  {
+    "revision": "c97c162f80e2002104cf9880a2e50dad",
+    "url": "/vue-calendar/index.html"
+  },
+  {
+    "revision": "aa9bb5b6bbbc111bd58b",
+    "url": "/vue-calendar/js/app.18a423dc.js"
+  },
+  {
+    "revision": "2772038c26b1f34dcd82",
+    "url": "/vue-calendar/js/chunk-vendors.bcd370bb.js"
+  },
+  {
+    "revision": "bed0c2973eaf9ef3e6751b28027b37e8",
+    "url": "/vue-calendar/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-calendar/robots.txt"
+  }
+]);
